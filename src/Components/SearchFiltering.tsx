046872a88bf4785c98f0f0d70d3e5d8f16f5a904
Anthony Sutton <anthony.sutton95@gmail.com>
2020-12-18 15:01:45 -0800

    feat(mobileResponse): Added mobile response for main page

--- src/Components/SearchFiltering.tsx
+++ src/Components/SearchFiltering.tsx
@@ -3,8 +3,8 @@ import { HiSearch } from 'react-icons/hi';
 
 const SearchFiltering = () => {
   return (
-    <div className="flex justify-between">
-      <div className="flex relative items-center shadow w-1/3">
+    <div className="flex flex-col md:flex-row justify-between">
+      <div className="flex relative items-center shadow md:w-1/3 w-full">
         <span className="absolute inset-y-0 left-0 flex items-center pl-2">
           <HiSearch className="text-gray-500" />
         </span>
@@ -14,7 +14,7 @@ const SearchFiltering = () => {
           className="py-2 pl-8 w-full rounded-lg border-none focus:ring-0"
         />
       </div>
-      <div>
+      <div className="mt-6 md:mt-0">
         <select className="rounded-lg shadow border-none focus:ring-0">
           <option value="">Filter by Region</option>
           <option value="africa">Africa</option>
